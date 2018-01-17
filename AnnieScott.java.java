6f0a0c3ecb9690b7c45851cf051f7f078149a17f
anniescott701 <anniescott701@gmail.com>
2018-01-17 17:18:38 +0530

    First commit

---
+++ AnnieScott.java.java
@@ -0,0 +1,9 @@
+package class1;
+
+public class AnnieScott {
+	public static void main(String[] args) {
+		System.out.print("my name is annie");
+		
+	}
+
+}
---
+++ helloworld_anniescott.java
@@ -0,0 +1,9 @@
+package class1;
+
+public class helloworldanniescott {
+	
+	public static void main(String[] args) {
+	System.out.print("hello world");
+	}
+
+}
